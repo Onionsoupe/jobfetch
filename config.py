182438bcf703201ea1eec75715031f8250c23d55
Ian Gabriel <911iangab44@gmail.com>
2026-09-05 22:02:15 +0200

    Added scraper for greenhouse with arx company

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from scraper import OpenRoboticsScraper, WorkdayJobScraper
+from scraper import OpenRoboticsScraper, WorkdayJobScraper, GreenhouseScraper
 
 def get_scrapers():
     """Returns all active scraper targets."""
@@ -8,5 +8,9 @@ def get_scrapers():
             company_name="rosennxt",
             wd_host="rosennxt.wd103.myworkdayjobs.com",
             site_id="rosenxt"
+        ),
+        GreenhouseScraper(
+            board_token="arxroboticsgmbh",
+            company_name="ARX Robotics"
         )
     ]
